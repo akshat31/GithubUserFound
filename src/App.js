7fbfd283647db8e394c6432be972e5f824ed96d1
akshat31 <akshat.soni31@gmail.com>
2018-08-03 18:29:26 +0530

    adding pagination

--- src/App.js
+++ src/App.js
@@ -10,10 +10,13 @@ import TableHead from "./component/tableHead";
 import UserImage from './component/UserImage';
 
 class App extends Component {
+
     state ={
         imgError:false,
         dataSet:[],
         showTable:false,
+        currentPage: 1,
+        reposPerPage: 10
     }
 
 
@@ -49,50 +52,81 @@ class App extends Component {
         )
     }
 
+    handleClick = (event) =>{
+        this.setState({
+          currentPage: Number(event.target.id)
+        });
+      }
+
     render() {
+
+        const { dataSet , currentPage, reposPerPage } = this.state;
+
+        // Logic for displaying current repos
+        const indexOfLastTodo = currentPage * reposPerPage;
+        const indexOfFirstTodo = indexOfLastTodo - reposPerPage;
+        const currentTodos = dataSet.slice(indexOfFirstTodo, indexOfLastTodo);
+
+        // Logic for displaying page numbers
+        const pageNumbers = [];
+        for (let i = 1; i <= Math.ceil(dataSet.length / reposPerPage); i++) {
+          pageNumbers.push(i);
+        }
+
+        const renderPageNumbers = pageNumbers.map(number => {
+            return (
+              <li
+                className="page-item"
+                key={number}
+              >
+              <a className="page-link" onClick={this.handleClick} id={number}>
+              {number}
+              </a>
+              </li>
+            );
+          });
 
     return (
         <div className="container mt-5">
          <div className="row">
+
             <div className="col-lg-4">
                 <Title/><br/>
                 <Form getDetail ={ this.getDetail }/>
                 <br/>
                 <br/>
             </div>
-            
             <div className="col-lg-4">
                 { this.state.showTable && this.getImage() }
             </div>
             <div className="col-lg-4">
                { 
                     this.state.dataSet && this.state.showTable && 
-                    // <table className="table">
-                    //     <tbody>
-                    //     <tr>
-                    //         <td>User Name</td>
-                    //         <td>{ this.state.dataSet[0].owner.login}</td>
-                    //     </tr>
-                    //     </tbody>
-                    // </table>
                     <h1>User Name :-<b>{ this.state.dataSet[0].owner.login}</b></h1>
                 }
             </div>
-            </div>
-            <div className="row mt-5">
-                <div className="col-md-12">
+
+        </div>
+
+        <div className="row mt-5">
+            <div className="col-md-12">
                 { this.state.imgError && this.errorimg() }
-                    <table className="table table-striped table-bordered table-light">
-                        <thead className="thead-dark">
-                            { this.state.showTable && <TableHead/> }
-                        </thead>        
-                        <tbody>
-                            {this.state.dataSet && this.state.dataSet.map((row) => <UserDetail  
-                        key= {row.id} data = {row}/>)}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+                <table className="table table-striped table-bordered table-light">
+                    <thead className="thead-dark">
+                        { this.state.showTable && <TableHead/> }
+                    </thead>        
+                    <tbody>
+                        {this.state.dataSet && currentTodos.map((row) => <UserDetail  
+                    key= {row.id} data = {row}/>)}
+                    </tbody>
+                </table>
+            </div>       
+        </div>
+            { this.state.dataSet.length > 10 && 
+                <ul className="pagination" style={{float:'right'}}>
+                    {renderPageNumbers}
+                </ul>
+            }
         </div>
     );
   }
